bcfbeced38892ba6180cb59bbd20a97e8c610099
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-07-31 17:42:47 -0400

    Adjust parameters

--- src/game.js
+++ src/game.js
@@ -1,8 +1,8 @@
 class Game {
-  constructor(personObj1, playerObj2) {      //possibly just pass through person obj
-    this.p1Wins = personObj1.wins       // also just p1 token and p2 just like this
-    this.p2Wins = personObj2.wins
-    this.turn = turn; //this is counter could be boolean value, could be number
+  constructor(player1, player2) {
+    this.p1Wins = player1.wins
+    this.p2Wins = player2.wins
+    this.turn = 0;                    /// increment, every % 2 === 0 is second players turn
     this.placedIconLocation = [       //an array containing 9 elements, manipulate these elements to show right icon, by default hidden
       one,                            // may have to be objects
       two,
@@ -15,11 +15,12 @@ class Game {
       nine
     ];
   }
+
   placeIcon() {
-    // adjust element in array at resepective location to respective icon
-    // if player 1 placedIconLocation[i].whodunnit = p1 if play 2 opposite
-    // check if 1,5,9 = p1, win
-    // if turn = 8 // draw
-    // if win, p1/2.saveWinsToStorage
+            // adjust element in array at resepective location to respective icon
+                // if player 1 placedIconLocation[i].whodunnit = p1 if play 2 opposite
+                    // check if 1,5,9 = p1, win
+                        // if turn = 8 // draw
+                            // if win, p1/2.saveWinsToStorage
   }
 };
