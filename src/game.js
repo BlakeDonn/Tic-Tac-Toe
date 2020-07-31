4fbe2a7b2e4396c78e0eeef26de00ae1508f93dc
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-07-31 12:52:16 -0400

    Add more pseduocode, refine functions

--- src/game.js
+++ src/game.js
@@ -2,8 +2,18 @@ class Game {
   constructor(personObj1, playerObj2) {      //possibly just pass through person obj
     this.p1Wins = personObj1.wins       // also just p1 token and p2 just like this
     this.p2Wins = personObj2.wins
-    this.placedIconLocation = [];       //an array containing 9 elements, manipulate these elements to show right icon, by default hidden
-    this.turn = turn;                   //this is counter could be boolean value, could be number
+    this.turn = turn; //this is counter could be boolean value, could be number
+    this.placedIconLocation = [       //an array containing 9 elements, manipulate these elements to show right icon, by default hidden
+      one,                            // may have to be objects
+      two,
+      three,
+      four,
+      five,
+      six,
+      seven,
+      eight,
+      nine
+    ];
   }
   placeIcon() {
     // adjust element in array at resepective location to respective icon
---
+++ src/main.js
@@ -0,0 +1,2 @@
+have a function hold a variable that holds an instantiation of the game class,
+can reference this for all the relevant game information, and can use this to assert game winning conditions
