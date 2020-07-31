c28c7283eea0f0061ae41d9c8f8599fb9e4eb544
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-07-30 22:14:53 -0400

    Add framework for class with properties and methods with relevant pseudocode

--- src/player.js
+++ src/player.js
@@ -2,12 +2,13 @@ class Player {
   constructor(id, token, wins) {
     this.id = id;
     this.token = token;
-    this. wins = wins;
+    this.wins = wins || 0;
   }
   saveWinsToStorage() {
     // On every game end, add new wincount to local storage
   }
-  retrieveWinsToStorage() {
+  retrieveWinsFromStorage() {
     // on load, pull from local storage and instantiate the two playesr with the relevant information
+    // Player.retrieve to access this
   }
 };
