df0ebea7652d0ca1ed44f497ce69269d0d6825e9
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-08-01 00:04:33 -0400

    Add theorized functionality for placeToken function, adjust changeTurn function

--- src/game.js
+++ src/game.js
@@ -1,7 +1,7 @@
 class Game {
   constructor(player1, player2) {
-    // this.p1Wins = player1.wins
-    // this.p2Wins = player2.wins
+    this.p1Turn = player1.wins > player2.wins ? 1 : 2;
+    this.p2Turn = player2.wins > player1.wins ? 1 : 2;
     this.turn = 1;
     this.placedIconLocation = [
       '',
@@ -15,13 +15,19 @@ class Game {
       ''
     ];
   }
-
+  placeToken(player1, player2) {
+    if (this.turn === 1) {
+      return this.p1Turn === 1 ? player1.token : player2.token;
+    }
+    if (this.turn > 1) {
+      return this.turn % 2 === 0 && this.p1Turn === 1 ? player2.token : player1.token;
+    }
+  }
   changeTurn() {
     if (this.turn > 8) {
       return
     }
     this.turn++
-    var whichPlayer = this.turn % 2 === 0 ? 1 : 2;
   }
   checkWinCondition() {
     for (var i = 0; i < 9; i ++) {
