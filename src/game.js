6d7c4673f3213b2ea43acdddfd22b5f567ef3e13
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-08-01 03:38:33 -0400

    Refactor placeToken to have better access to relevant values, create updatePlacedIconLocation to adjust board array

--- src/game.js
+++ src/game.js
@@ -1,6 +1,8 @@
 class Game {
   constructor(player1, player2) {
-    this.p1Turn = player1.wins > player2.wins ? 1 : 2;
+    this.player1 = player1;
+    this.player2 = player2;
+    this.p1Turn = player1.wins >= player2.wins ? 1 : 2;
     this.p2Turn = player2.wins > player1.wins ? 1 : 2;
     this.turn = 1;
     this.placedIconLocation = [
@@ -15,15 +17,21 @@ class Game {
       ''
     ];
   }
-  placeToken(player1, player2) {
+  placeToken() {
     if (this.turn === 1) {
-      this.changeTurn()
-      return this.p1Turn === 1 ? player1.token : player2.token;
+      this.turn++
+      var firstMovePlace = this.p1Turn === 1 ? this.p1Turn : this.p2Turn;
+      return firstMovePlace;
     }
     if (this.turn > 1) {
-      var assetToReturn = this.turn % 2 === 0 && this.p1Turn === 1 ? player2.token :  player1.token;
+      var restOfGamePlace = this.turn % 2 === 0 && this.p1Turn === 1 ? this.p2Turn :  this.p1Turn;
+      return restOfGamePlace;
     }
   }
+  updatePlacedIconLocation(test, player) {
+    this.placedIconLocation[test] = player;
+  }
+
   changeTurn() {
     if (this.turn > 8) {
       return
@@ -38,4 +46,3 @@ class Game {
     }
   }
 };
-module.exports = Game;
