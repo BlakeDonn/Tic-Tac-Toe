77fe2f4f4b4a94947b95c55b5b682cc590919e93
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-08-04 00:00:42 -0400

    Add event listeners, add display and update player Data

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 var gameBoard = document.querySelector(".board");
+var player1DOM = document.querySelector(".player1")
+var player2DOM = document.querySelector(".player2")
+var turnToken = document.querySelector(".turn-token")
+var turnToken2 = document.querySelector(".turn-token2")
 
 // Data Model
 var currentGame = '';
@@ -6,11 +10,13 @@ var currentGame = '';
 //Event listeners
 window.addEventListener("load", createGame);
 gameBoard.addEventListener("mousedown", playerMove);
+gameBoard.addEventListener("mouseup", updatePlayerData);
 
 //Event Handler
 function createGame() {
   var players = Player.retrieveWinsFromStorage('savedPlayers');
   currentGame = new Game(players[0], players[1]);
+  displayPlayerData(currentGame.player1, currentGame.player2)
 };
 
 function playerMove(){
@@ -25,4 +31,16 @@ function updateBoard(asset){
   var insertedHTML = `<img class = "board-token" src = ${asset}>`
   event.target.closest(".icon").insertAdjacentHTML("afterbegin", insertedHTML)
   currentGame.checkWinCondition();
+}
+
+function displayPlayerData(player1, player2){
+  var p1Token = `<img class = "token1" src = "${player1.token}">`
+  player1DOM.insertAdjacentHTML("afterbegin", p1Token);
+  var p2Token = `<img class = "token2" src = "${player2.token}">`
+  player2DOM.insertAdjacentHTML("afterbegin", p2Token);
+
+}
+function updatePlayerData(player1, player2){
+  turnToken.classList.toggle("hidden");
+  turnToken2.classList.toggle("hidden");
 }
