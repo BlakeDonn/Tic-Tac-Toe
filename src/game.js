b506bceeb790259eba758368d22d9647e992481b
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-08-04 18:08:59 -0400

    Combine placeToken and changeTurn function

--- src/game.js
+++ src/game.js
@@ -5,34 +5,28 @@ class Game {
     this.turn = 1;
     this.placedIconLocation = ['', '', '', '', '', '', '', '', ''];
   }
-  placeToken() {
-    return this.turn % 2 == 0 ? 2 : 1;
-  }
 
   updatePlacedIconLocation(index, player) {
     this.placedIconLocation[index] = player;
-    return player === 1 ? this.player1.token : this.player2.token;
   }
 
   changeTurn() {
-    if (this.turn > 9) {
-      displayDraw();
-    }
-    else this.turn++
+    return this.turn % 2 == 0 ? (this.turn++, this.player2.token) : (this.turn++, this.player1.token);
   }
   checkWinCondition(player) {
+    console.log(player)
     for (var i = 0; i < 9; i ++) {
       if (i == 0 || i == 3 || i == 6){
-      var one = this.placedIconLocation[i] == player && this.placedIconLocation[i + 1] == player && this.placedIconLocation[i + 2] == player ? `${player} wins!`: null;
+      var one = this.placedIconLocation[i] == player && this.placedIconLocation[i + 1] == player && this.placedIconLocation[i + 2] == player ? player : null;
       }
       if (i == 0 || i == 1 || i == 2){
-      var two =  this.placedIconLocation[i] == player && this.placedIconLocation[i + 3] == player && this.placedIconLocation[i + 6] == player ? `${player} wins!`: null;
+      var two =  this.placedIconLocation[i] == player && this.placedIconLocation[i + 3] == player && this.placedIconLocation[i + 6] == player ? player: null;
       }
       if (i == 6 ){
-      var three =  this.placedIconLocation[i] == player && this.placedIconLocation[i - 2] == player && this.placedIconLocation[i - 4] == player ? `${player} wins!`: null;
+      var three =  this.placedIconLocation[i] == player && this.placedIconLocation[i - 2] == player && this.placedIconLocation[i - 4] == player ? player: null;
       }
       if (i == 8){
-      var four =  this.placedIconLocation[i] == player && this.placedIconLocation[i - 4] == player && this.placedIconLocation[i - 8] == player ? `${player} wins!`: null;
+      var four =  this.placedIconLocation[i] == player && this.placedIconLocation[i - 4] == player && this.placedIconLocation[i - 8] == player ? player: null;
       }
       if (one != null){
         return one;
@@ -49,14 +43,13 @@ class Game {
     }
   }
 
-
   resetBoard(){
     this.placedIconLocation = ['', '', '', '', '', '', '', '', '']
     this.turn = 1;
   }
 
   updatePlayerWins(result){
-    result.includes(1) ? this.player1.wins++ : this.player2.wins++;
+    result.includes(this.player1.token) ? this.player1.wins++ : this.player2.wins++;
     Player.saveWinsToStorage('savedPlayers', [this.player1, this.player2])
     displayPlayerData(this.player1, this.player2);
   }
