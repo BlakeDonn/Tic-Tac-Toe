5df11d0258518d190b9f3d714f93038afc18cf02
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-08-04 00:25:29 -0400

    Add query selectors for wins, adjust parameters, add wins innertext

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ var player1DOM = document.querySelector(".player1")
 var player2DOM = document.querySelector(".player2")
 var turnToken = document.querySelector(".turn-token")
 var turnToken2 = document.querySelector(".turn-token2")
+var player1Wins = document.querySelector(".wins1")
+var player2Wins = document.querySelector(".wins2")
 
 // Data Model
 var currentGame = '';
@@ -23,14 +25,15 @@ function playerMove(){
   var boardLocation = event.target.id;
   var currentPlayer = currentGame.placeToken();
   var boardLocationToChange = currentGame.updatePlacedIconLocation(boardLocation, currentPlayer);
-  updateBoard(boardLocationToChange);
+  updateBoard(boardLocationToChange, currentPlayer);
   currentGame.changeTurn();
+
 };
 
-function updateBoard(asset){
+function updateBoard(asset, currentPlayer){
   var insertedHTML = `<img class = "board-token" src = ${asset}>`
   event.target.closest(".icon").insertAdjacentHTML("afterbegin", insertedHTML)
-  currentGame.checkWinCondition();
+  currentGame.checkWinCondition(currentPlayer);
 }
 
 function displayPlayerData(player1, player2){
@@ -38,8 +41,10 @@ function displayPlayerData(player1, player2){
   player1DOM.insertAdjacentHTML("afterbegin", p1Token);
   var p2Token = `<img class = "token2" src = "${player2.token}">`
   player2DOM.insertAdjacentHTML("afterbegin", p2Token);
-
+  player1Wins.innerText = `${player1.wins} wins`;
+  player2Wins.innerText = `${player2.wins} wins`;
 }
+
 function updatePlayerData(player1, player2){
   turnToken.classList.toggle("hidden");
   turnToken2.classList.toggle("hidden");
