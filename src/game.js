6ed5a393f54dd94bc5682d471e30ed77e9d52cb8
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-08-04 19:06:51 -0400

    Refactor evaluateResult to move DOM altering functions to main

--- src/game.js
+++ src/game.js
@@ -3,54 +3,64 @@ class Game {
     this.player1 = new Player(player1);
     this.player2 = new Player(player2);
     this.turn = 1;
-    this.placedIconLocation = ['', '', '', '', '', '', '', '', ''];
-  }
-
-  updatePlacedIconLocation(index, player) {
-    this.placedIconLocation[index] = player;
+    this.boardLayout = ['', '', '', '', '', '', '', '', ''];
   }
 
   changeTurn() {
     return this.turn % 2 == 0 ? (this.turn++, this.player2.token) : (this.turn++, this.player1.token);
   }
+
+  updateBoardLayout(index, player) {
+    this.boardLayout[index] = player;
+  }
+
   checkWinCondition(player) {
-    console.log(player)
     for (var i = 0; i < 9; i ++) {
-      if (i == 0 || i == 3 || i == 6){
-      var one = this.placedIconLocation[i] == player && this.placedIconLocation[i + 1] == player && this.placedIconLocation[i + 2] == player ? player : null;
+      if (i == 0 || i == 3 || i == 6) {
+        var one = this.boardLayout[i] == player && this.boardLayout[i + 1] == player && this.boardLayout[i + 2] == player ? player : null;
       }
-      if (i == 0 || i == 1 || i == 2){
-      var two =  this.placedIconLocation[i] == player && this.placedIconLocation[i + 3] == player && this.placedIconLocation[i + 6] == player ? player: null;
+      if (i == 0 || i == 1 || i == 2) {
+        var two =  this.boardLayout[i] == player && this.boardLayout[i + 3] == player && this.boardLayout[i + 6] == player ? player: null;
       }
-      if (i == 6 ){
-      var three =  this.placedIconLocation[i] == player && this.placedIconLocation[i - 2] == player && this.placedIconLocation[i - 4] == player ? player: null;
+      if (i == 6 ) {
+        var three =  this.boardLayout[i] == player && this.boardLayout[i - 2] == player && this.boardLayout[i - 4] == player ? player: null;
       }
-      if (i == 8){
-      var four =  this.placedIconLocation[i] == player && this.placedIconLocation[i - 4] == player && this.placedIconLocation[i - 8] == player ? player: null;
+      if (i == 8) {
+        var four =  this.boardLayout[i] == player && this.boardLayout[i - 4] == player && this.boardLayout[i - 8] == player ? player: null;
       }
-      if (one != null){
+      if (one != null) {
         return one;
       }
-      if (two != null){
+      if (two != null) {
         return two;
       }
-      if (three != null){
+      if (three != null) {
         return three;
       }
-      if (four != null){
+      if (four != null) {
         return four;
       }
     }
   }
 
-  resetBoard(){
-    this.placedIconLocation = ['', '', '', '', '', '', '', '', '']
+  evaluateResult(result) {
+    if (this.turn === 10 && result === undefined) {
+      return false;
+    }
+    if (result === this.player1.token || result === this.player2.token) {
+      return true;
+    }
+  }
+
+  updatePlayerWins(result) {
+    result.includes(this.player1.token) ? this.player1.wins++ : this.player2.wins++;
+    Player.saveWinsToStorage('savedPlayers', [this.player1, this.player2]);
+    displayPlayerData(this.player1, this.player2);
+  }
+
+  resetBoard() {
+    this.boardLayout = ['', '', '', '', '', '', '', '', ''];
     this.turn = 1;
   }
 
-  updatePlayerWins(result){
-    result.includes(this.player1.token) ? this.player1.wins++ : this.player2.wins++;
-    Player.saveWinsToStorage('savedPlayers', [this.player1, this.player2])
-    displayPlayerData(this.player1, this.player2);
-  }
 };
